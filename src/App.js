20404fc2fbcee4e9297f94450b37e30e444db6bf
Jonas.J <jan.jonas.johansson@gmail.com>
2017-10-20 23:10:55 +0200

    saven so Johans code can be viewed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import './registerServiceWorker';
 import Logo from './logo';
 import './App.css';
 import { Link } from 'inferno-router';
+import Header from './header/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo width="80" height="80" />
-          <h1>{`Welcome to Inferno ${version}`}</h1>
-        </header>
+      <div className="App">     
+        <Header/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+import Component from 'inferno-component';
+import './header.css';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="header">
+        <p>THis is a header</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
