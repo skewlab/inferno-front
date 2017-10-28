8691d333d413b802cc54396efb2bb36a7d1cd32b
fippli <filip.carl.johansson@gmail.com>
2017-10-28 15:58:08 +0200

    Removed some outcommented code

--- src/utils/ApiService.js
+++ src/utils/ApiService.js
@@ -92,6 +92,7 @@ const ApiService = (function () {
 	    handleError( { message: 'Response was not JSON' } );
 	  }
 	}
+	
 	// Handle fetch errors
 	function handleError( error ) {
 	  console.error( 'An error occurred:', error );
@@ -102,109 +103,5 @@ const ApiService = (function () {
 
 })();
 
-// class ApiService {
-//
-// 	conf() {
-// 		console.log( "Config.API" );
-// 	}
-
-	/*
-		Get any endpoint in the api
-	*/
-	// get( endpoint ) {
-	// 	return fetch( Config.API + endpoint ,
-	// 		{
-	// 			method: "GET"
-	// 		})
-	// 		.then(
-	// 			this._verifyResponse,
-	// 			this._handleError );
-	// };
-	//
-	// /*
-	// 	Post to any endpoint in the api
-	// */
-	// post( endpoint, data ) {
-	// 	return fetch( Config.API + endpoint ,
-	// 		{
-	//     	method: "POST",
-	//     	headers: {
-	//       	"Content-type": "application/json"
-	//     	},
-	//     	body: JSON.stringify( data )
-	//   	})
-	// 		.then(
-	// 			this._verifyResponse,
-	// 			this._handleError );
-	// };
-	//
-	// /*
-	// 	Update data on any endpoint
-	// */
-	// put( endpoint, data ) {
-	// 	return fetch( Config.API + endpoint,
-	// 		{
-	//     	method: "PUT",
-	//     	headers: {
-	//       	"Content-type": "application/json"
-	//     	},
-	//     	body: JSON.stringify( data )
-	//   	})
-	// 		.then(
-	// 			this._verifyResponse,
-	// 			this._handleError );
-	// };
-	//
-	// /*
-	// 	Delete from any endpoint
-	// */
-	// delete( endpoint ) {
-	// 	return fetch( Config.API + endpoint,
-	// 		{
-	// 			method: "DELETE"
-	// 		})
-	// 		.then(
-	// 			this._verifyResponse,
-	// 			this._handleError );
-	// };
-	//
-	// // Verify that the fetched response is JSON
-	// _verifyResponse( res ) {
-	//   let contentType = res.headers.get('content-type');
-	//   if (contentType && contentType.indexOf('application/json') !== -1) {
-	//     return res.json();
-	//   } else {
-	//     this._handleError({ message: 'Response was not JSON'});
-	//   }
-	// }
-	// // Handle fetch errors
-	// _handleError( error ) {
-	//   console.error('An error occurred:', error);
-	//   throw error;
-	// }
-
-	// signUp( signUpCredentials ) {
-	//   return fetch(`${API}users`,{
-	//     method: "POST",
-	//     headers: {
-	//       "Content-type": "application/json"
-	//     },
-	//     body: JSON.stringify( signUpCredentials )
-	//   })
-	// .then(this._verifyResponse, this._handleError);
-	// }
-	//
-	// signIn( signInCredentials ) {
-	//   return fetch(`${API}signin`,{
-	//     method: "POST",
-	//     headers: {
-	//       "Content-type": "application/json"
-	//     },
-	//     body: JSON.stringify(signInCredentials)
-	//   })
-	//   .then(this._verifyResponse, this._handleError);
-	// }
-
-// }
 
 export default ApiService;
