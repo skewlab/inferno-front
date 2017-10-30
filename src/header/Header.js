1a1e5bb7134de409b73419fc29d5630b76585311
jonasj <jonasj9@kth.se>
2017-10-30 12:42:11 +0100

    sign out functionallity added in header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,19 +1,36 @@
 /*
-Authors:
-Email:
-Description:
+  Author: Jonas Johansson
+  Email:  jan.jonas.johansson@gmail.com
+  Github: jjojo
+  Description:
+  Header component for the top menu
 */
+
 
 import Component from 'inferno-component';
 import { Link } from 'inferno-router';
+import ApiService from '../utils/ApiService';
 import './header.css';
 
 
 class Header extends Component {
 
+  
   /* For future implementation when user can sign in and not */
   isLoggedIn() {
     return true;
+  }
+
+  signOut( ){
+    ApiService.post( "signout" ).then(
+      res => {
+        console.log(res)
+        //need to lear how to route with inferno router...
+        document.location.href = "/";
+      }, error => {
+        console.log(error)
+      }
+    )
   }
 
 
@@ -21,7 +38,7 @@ class Header extends Component {
   signedInHeader() {
     return (
       <header>
-        <Link to="/signout" className="link-right link">Sign out</Link>
+        <a onClick={ this.signOut } className="link-right link">Sign out</a>
         <Link to="/my-profile" className="link-right link" >My profile</Link>
         <Link to="/start" className="link-left link">Start</Link>
       </header>
